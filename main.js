51161213d4425027ce7a9a31f6e7e5bf8b4d129f
Whoistolu <digittolu25@gmail.com>
2021-09-01 10:45:56 +0100

    Add some testrun

---
+++ main.js
@@ -0,0 +1,37 @@
+const btn = document.querySelector('#click-button');
+function display () {
+    const selectedBox = document.querySelector('.box');
+    selectedBox.classList.add('show');
+}
+
+btn.addEventListener('click',display);
+
+
+const closeButton = document.querySelector('#x-button')
+function closePop () {
+    const selectedBox2 = document.querySelector('.box');
+    selectedBox2.classList.remove('show');
+}
+closeButton.addEventListener('click',closePop);
+
+
+
+
+
+function display () {
+    const targetedDiv = document.querySelector('.mobile-menu-hidden');
+    targetedDiv.classList.add('show');
+    menuButton.style.display = 'none';
+}
+menuButton.addEventListener('click',display);
+const closeButton = document.querySelector('#close-button');
+function closePop () {
+    const targetedDiv2 = document.querySelector('.mobile-menu-hidden');
+    targetedDiv2.classList.remove('show');
+    menuButton.style.display = 'block';
+}
+closeButton.addEventListener('click',closePop);
+const anchorTagLinks = document.querySelectorAll('.menu-links');
+anchorTagLinks.forEach((link) => {
+    link.addEventListener('click', closePop);
+})
