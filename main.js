4097176793d716e31ca229697d3a4ba79f519773
Whoistolu <digittolu25@gmail.com>
2021-09-04 00:29:29 +0100

    .

--- main.js
+++ main.js
@@ -12,13 +12,13 @@ function closePop () {
     const selectedBox2 = document.querySelector('.box');
     selectedBox2.classList.remove('show');
 }
-closeButton.addEventListener('click',closePop);
+closeButton.addEventListener('click',closePop)
 
 
 
 
 
-function display () {
+function display ( {
     const targetedDiv = document.querySelector('.mobile-menu-hidden');
     targetedDiv.classList.add('show');
     menuButton.style.display = 'none';
@@ -33,5 +33,5 @@ function closePop () {
 closeButton.addEventListener('click',closePop);
 const anchorTagLinks = document.querySelectorAll('.menu-links');
 anchorTagLinks.forEach((link) => {
-    link.addEventListener('click', closePop);
+    link.addEventListener('', closePop);
 })
